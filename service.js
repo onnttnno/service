2693d6603ace8e962250848010b70305fe7afc9e
user <user@PATA20.local>
2018-03-23 15:21:32 +0700

    add ejs and singleton

--- service.js
+++ service.js
@@ -5,91 +5,117 @@ var $ = require('jQuery');
 var jose = require('node-jose');
 var keystore = jose.JWK.createKeyStore();
 
-var props = {
-    kid: 'ServiceKeys',
-    alg: 'A256GCM',
-    use: 'enc'
-};
-keystore.generate("oct", 1024, props).
-then(function (result) {
-    // {result} is a jose.JWK.Key
-    key = result;
-    console.log("generate Key completed");
-});
-
-
-
-function init(api, token) {
-    //request public key
-    https.get('http://[ip_address]/getPublicJey', (resp) => {
-
-        // A chunk of data has been recieved.
-        resp.on('data', (key) => {
-            keystore.add(key, "pem").
-            then(function (result) {
-                // {result} is a jose.JWK.Key
-                handshake(api, token);
-            });
-
-        });
-    }).on("error", (err) => {
-        console.log("Error: " + err.message);
-    });
-
-}
-
-function handshake(api, token) {
-    let data = {
-        "api": api,
-        "token": token,
-        "pK": keystore.get('ServiceKeys').toPEM()
+var service = function service(){
+    var props = {
+        kid: 'ServiceKeys',
+        alg: 'A256GCM',
+        use: 'enc'
     };
-    jose.JWE.createEncrypt(keystore.get('ServerKey')).
-    update(data).
-    final().
-    then(function (cypher) {
-        // {cypher} is a JSON Object -- JWE using the JSON General Serialization
+    keystore.generate("oct", 1024, props).
+    then(function (result) {
+        // {result} is a jose.JWK.Key
+        key = result;
+        console.log("generate Key completed");
+    });
 
-        https.post('http://localhost/handShake/' + cypher, (resp) => {
+
+
+    this.init =function(api, token) {
+        //request public key
+        https.get('http://[ip_address]/getPublicJey', (resp) => {
 
             // A chunk of data has been recieved.
-            resp.on('data', (dat) => {
-                console.log(dat);
-                jose.JWE.createDecrypt(keystore.get('ServiceKeys')).
-                decrypt(dat).
+            resp.on('data', (key) => {
+                keystore.add(key, "pem").
                 then(function (result) {
-                    console.log(result);
+                    // {result} is a jose.JWK.Key
+                   return handshake(api, token);
                 });
-            });
 
+            });
         }).on("error", (err) => {
             console.log("Error: " + err.message);
+            return err;
         });
 
+    }
 
-    });
+    function handshake(api, token) {
+        let data = {
+            "api": api,
+            "token": token,
+            "pK": keystore.get('ServiceKeys').toPEM()
+        };
+        jose.JWE.createEncrypt(keystore.get('ServerKey')).
+        update(data).
+        final().
+        then(function (cypher) {
+            // {cypher} is a JSON Object -- JWE using the JSON General Serialization
+
+            https.post('http://localhost/handShake/' + cypher, (resp) => {
+
+                // A chunk of data has been recieved.
+                resp.on('data', (dat) => {
+                    console.log(dat);
+                    jose.JWE.createDecrypt(keystore.get('ServiceKeys')).
+                    decrypt(dat).
+                    then(function (result) {
+                        console.log(result);
+                        return result;
+                    });
+                });
+
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+                return err;
+            });
+
+
+        });
+
+    }
+
+    this.load = function(url) {
+        jose.JWE.createEncrypt(keystore.get('ServerKey')).
+        update(url).
+        final().
+        then(function (cypher) {
+            // {cypher} is a JSON Object -- JWE using the JSON General Serialization
+
+            https.get('https://localhost/load/' + cypher, (resp) => {
+
+                // A chunk of data has been recieved.
+                resp.on('data', (dat) => {
+                    console.log(dat);
+                    resp.render('candlechart',{items:dat});
+                    return "open complete";
+                });
+
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+                return err;
+            });
+
+
+        });
+
+    }
 
 }
 
-function load(url) {
-    jose.JWE.createEncrypt(keystore.get('ServerKey')).
-    update(url).
-    final().
-    then(function (cypher) {
-        // {cypher} is a JSON Object -- JWE using the JSON General Serialization
-
-        https.get('https://localhost/load/' + cypher, (resp) => {
-
-            // A chunk of data has been recieved.
-            resp.on('data', (dat) => {
-                console.log(dat);
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
 
 
-    });
-
+service.instance = null;
+ 
+/**
+ * Singleton getInstance definition
+ * @return singleton class
+ */
+service.getInstance = function(){
+    if(this.instance === null){
+        this.instance = new service();
+    }
+    return this.instance;
 }
+ 
+module.exports = service.getInstance();
